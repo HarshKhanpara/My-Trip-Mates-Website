8fd37b2b88da9e0f87cc385d55cb51c6ecd536fc
Harsh <khanparaharsh2003@gmail.com>
2024-09-16 11:12:36 +0530

    Testing metadata

--- app/about/layout.js
+++ app/about/layout.js
@@ -1,28 +1,27 @@
-
 export const metadata = {
     title: 'About Us - Crafting Travel Experiences',
     description: 'Learn about our passion for crafting extraordinary travel experiences. Discover our beliefs and commitment to customer satisfaction, sustainability, and authenticity.',
-    siteName:'My Trip Mates',
+    siteName: 'My Trip Mates',
     openGraph: {
       title: 'About Us - Crafting Travel Experiences',
       description: 'Discover our mission and values. We are committed to creating memorable travel experiences while prioritizing sustainability, authenticity, and customer satisfaction.',
-      images:[ 
+      images: [
         {
-        url:'/gallery/background.jpg',
-        width: 800,
-        hieght:600
+          url: '/gallery/background.jpg',
+          width: 800,
+          height: 600 // Corrected spelling
         }
-    ]
-    },
+      ]
+    }
   };
-  
   
   const AboutLayout = ({children}) => {
     return (
       <>
         {children}
       </>
-    )
-  }
+    );
+  };
   
   export default AboutLayout;
+  
--- app/upcoming-trips/layout.js
+++ app/upcoming-trips/layout.js
@@ -4,6 +4,18 @@
 export const metadata = {
     title: 'Group Trips - Explore Your Next Adventure',
     description: 'Discover our upcoming group trips and explore new destinations with Fellow travelers. Book your next adventure today!',
+    siteName: 'My Trip Mates',
+    openGraph: {
+      title: 'Group Trips - Explore Your Next Adventure',
+      description: 'Join our upcoming group trips and explore new destinations with fellow travelers. Book your next adventure today!',
+      images: [
+        {
+          url: '/gallery/background.jpg',
+          width: 800,
+          height: 600
+        }
+      ]
+    }
 };
 
 export default function Layout({ children }) {
